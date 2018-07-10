120ea19397173f308c3a733e71e88cabd2d094d4
Alexander Praschek <admin@asumaone.com>
2018-07-10 18:32:47 +0200

    try-catch-Block hinzufügen bei Webservice
    Former-commit-id: e9aef2f65d6ee40a29ffc631b90a420bebb5189c

--- WoPeD-Process2Text/src/processtotext/ws/ProcessToTextWebService.java
+++ WoPeD-Process2Text/src/processtotext/ws/ProcessToTextWebService.java
@@ -16,28 +16,28 @@ import javax.xml.ws.WebServiceContext;
 import java.io.File;
 import java.io.PrintWriter;
 
+@SuppressWarnings("unused")
 @WebService
 public class ProcessToTextWebService {
     @Resource
     private WebServiceContext context;
 
-    public ProcessToTextWebService() {
-    }
-
     @SuppressWarnings({"unused", "ResultOfMethodCallIgnored"})
-    @WebMethod(
-            operationName = "generateTextFromProcessSpecification"
-    )
-    public String generateTextFromProcessSpecification(@WebParam(name = "processSpecification") String processSpecification) throws Exception {
-        File tempFile = File.createTempFile("TempXmlDat", ".pnml");
-        PrintWriter writer = new PrintWriter(tempFile, "UTF-8");
-        writer.println(processSpecification);
-        writer.close();
-        ServletContext servletContext = (ServletContext) this.context.getMessageContext().get("javax.xml.ws.servlet.context");
-        String contextPath = servletContext.getRealPath("/");
-        TextGenerator textGenerator = new TextGenerator(contextPath);
-        String result = textGenerator.toText(tempFile.getAbsolutePath());
-        tempFile.delete();
-        return result;
+    @WebMethod(operationName = "generateTextFromProcessSpecification")
+    public String generateTextFromProcessSpecification(@WebParam(name = "processSpecification") String processSpecification) {
+        try {
+            File tempFile = File.createTempFile("TempXmlDat", ".pnml");
+            PrintWriter writer = new PrintWriter(tempFile, "UTF-8");
+            writer.println(processSpecification);
+            writer.close();
+            ServletContext servletContext = (ServletContext) this.context.getMessageContext().get("javax.xml.ws.servlet.context");
+            String contextPath = servletContext.getRealPath("/");
+            TextGenerator textGenerator = new TextGenerator(contextPath);
+            String result = textGenerator.toText(tempFile.getAbsolutePath());
+            tempFile.delete();
+            return result;
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
